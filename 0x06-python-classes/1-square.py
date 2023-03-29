20734615fce3bc3749d7a1f3573d80820acf5d59
RyanNdegwa <ryanndegwa3560@gmail.com>
2023-03-29 18:03:17 +0000

    Python - Classes and Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module contains a class Square"""
+
+
+class Square:
+    """This class defines a square"""
+
+    def __init__(self, size):
+        """Initializes an instance of Square"""
+        self.__size = size
+
